c5d156813d23d510452a87317e1f070469ed6052
hollow <hollowcg@foxmail.com>
2018-08-28 16:47:59 +0800

    BaseController

--- application/admin/controller/BaseController.php
+++ application/admin/controller/BaseController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 
+use app\admin\model\UserModel;
 use app\common\model\UserMenuModel;
 use think\Controller;
 
@@ -26,27 +27,24 @@ class BaseController extends Controller
 
         $module= $request->module();
 
-//        var_dump(phpinfo());die;
-//        $userId = session('userId');
-//
-//        $user = session("user");
+        $userId = session('userId');
 
-//        if($controller !='Login' || $module != 'admin' ){
-//            if( empty($userId) || empty($user) ){
-//                if(think\Request::instance()->isAjax()){
-//                    return json(array('code' => 2, 'data' => [], 'msg' => '请重新登录'));
-//                }else{
-//                    return $this->redirect('login/login');
-//                }
-//
-//            }
-//        }
+        $user = session("user");
 
+        if($controller !='Site' || $module != 'admin' ){
+            if( empty($userId) || empty($user) ){
+                if(\think\facade\Request::isAjax()){
+                    return json(array('code' => 2, 'data' => [], 'msg' => '请重新登录'));
+                }else{
+                    return $this->redirect('site/login');
+                }
 
-//        $this->userId = $userId;
+            }
+        }
+
+        $this->userId = $userId;
         //菜单初始化
         $menu = $this->initmenu();
-//var_dump($menu);die;
         //面包屑
         $bread = UserMenuModel::getCurrentMenu('admin');
         $menu_id = '';
@@ -59,18 +57,18 @@ class BaseController extends Controller
             ksort(self::$current_menu);
         }
         $this->assign('menu',$menu);
-//        $this->assign('current_user',$user);
+        $this->assign('current_user',$user);
         $this->assign('menu_id',$menu_id);
         $this->assign('menu_parent_id',$menu_parent_id);
         $this->assign('current_menu',self::$current_menu);
 
         //检验操作权限
-//        $menu_arr=explode(',',session("user.menuIds"));
-//        if(!empty($bread) && $bread['ismenu']){
-//            if(!in_array($menu_id,$menu_arr) && session("user.roleId") != 1 && $controller !='Login' && $controller !='Index'){
-//                $this->error('没有操作权限');
-//            }
-//        }
+        $menu_arr= UserModel::getUserRoleArr($user);
+        if(!empty($bread) && $bread['is_show']){
+            if(!in_array($menu_id,$menu_arr) && session("user.role_id") != 1 && $controller !='Site' && $controller !='Index'){
+                $this->error('没有操作权限');
+            }
+        }
 
     }
 
@@ -78,35 +76,33 @@ class BaseController extends Controller
      * 根据用户角色初始化菜单
      */
     function initmenu(){
-        $roleId=session("user.id");
+        $user = session('user');
+        $role_id = $user['role_id'];
         $menus=[];
-//        if (empty($roleId)) {
-//            return $menus;
-//        }
+        if (empty($role_id)) {
+            return $menus;
+        }
         $con = [
             ['level', '<', 3],
             ['model', '=', 'admin'],
         ];
-//        $con['level'] = ['<',3];
-//        $con['model'] = 'admin';
-//        if($roleId !=1){//如果非超级管理员
-//            $con['id'] = ['in',session("user.menuIds")];
-//        }
+        if($role_id !=1){//如果非超级管理员
+            $menu_arr= UserModel::getUserRoleArr($user);
+            $con[] = ['id','in',$menu_arr];
+        }
 
-//        $rlt=getDataList("userMenu",$con,"name as  title,url,icon,id,flag,level,parentId","level asc,sort desc");
         $rlt =UserMenuModel::where($con)->field(['name as  title','url','icon','id','is_show','level','parent_id'])->order(['level'=>'asc','sort'=>'desc'])->select()->toarray();
+
         foreach ($rlt as $k =>$v){
             if($v['level'] == 1 ){
                 $v['children'] = [];
                 $menus[$v['id']] = $v;
             }
             if($v['level'] == 2 ){
-//                var_dump($v);die;
                 $v['url'] = url($v['url']);
                 $menus[$v['parent_id']]['children'][] = $v;
             }
         }
-//        var_dump($menus);die;
         return $menus;
     }
 
