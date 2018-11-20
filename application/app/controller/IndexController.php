6a2f826f30159d22d518f38d4a09d0610c57f141
hollow <hollowcg@foxmail.com>
2018-11-20 14:17:52 +0800

    修改

--- application/app/controller/IndexController.php
+++ application/app/controller/IndexController.php
@@ -1,45 +1,66 @@
 <?php
 namespace app\app\controller;
 
-use app\common\model\CustomerModel;
+use app\common\model\OrderAttrModel;
+use app\common\model\ProductCategoryModel;
 
 class IndexController extends BaseController
 {
 
     /*
-     * 绑定用户
+     *商家推荐,热销,上新,
+     *加入会员,分享有礼,
      */
     public function index()
     {
         try{
-            $request = input('','','trim');
-            $this->validate($request, 'Customer.bind');
-
-            $customer = CustomerModel::where(['name' => $request['name']])->find();
-            if (is_null($customer)) {
-                exception('用户名错误');
-            }
-
-            if($customer['status']==2){
-                 exception('用户被冻结');
-            }
-
-            if (md5($request['password']) == $customer->password) {
-                //清除该openid之前绑定的用户
-                CustomerModel::update(['openid' => ''],['openid'=>$request['openId']]);
-                $customer->openid = $request['openId'];
-                $customer->save();
-                $customer = $customer->toArray();
-                unset($customer['password']);
-                return $this->succeed('绑定成功',$customer);
-            } else {
-                exception('密码错误');
-            }
+            $data = [];
+            $data['category'] = ProductCategoryModel::where(['level'=>1,'is_show'=>1])->order(['sort'=>'desc'])->field(['id','name'])->select();
+            $data['hot'] = OrderAttrModel::hotSale();
+            return $this->succeed('操作成功',$data);
         }catch(\Exception $e){
             return $this->fail($e->getMessage());
         }
 
     }
+
+
+
+
+//    /*
+//     * 绑定用户
+//     */
+//    public function index()
+//    {
+//        try{
+//            $request = input('','','trim');
+//            $this->validate($request, 'Customer.bind');
+//
+//            $customer = CustomerModel::where(['name' => $request['name']])->find();
+//            if (is_null($customer)) {
+//                exception('用户名错误');
+//            }
+//
+//            if($customer['status']==2){
+//                 exception('用户被冻结');
+//            }
+//
+//            if (md5($request['password']) == $customer->password) {
+//                //清除该openid之前绑定的用户
+//                CustomerModel::update(['openid' => ''],['openid'=>$request['openId']]);
+//                $customer->openid = $request['openId'];
+//                $customer->save();
+//                $customer = $customer->toArray();
+//                unset($customer['password']);
+//                return $this->succeed('绑定成功',$customer);
+//            } else {
+//                exception('密码错误');
+//            }
+//        }catch(\Exception $e){
+//            return $this->fail($e->getMessage());
+//        }
+//
+//    }
 
 
 
--- application/app/controller/ProductController.php
+++ application/app/controller/ProductController.php
@@ -1,29 +1,86 @@
 <?php
 namespace app\app\controller;
 
-use app\common\model\CustomerModel;
+use app\common\model\ProductAttrModel;
 use app\common\model\ProductCategoryModel;
 use app\common\model\ProductModel;
 
 class ProductController extends BaseController
 {
 
-    /*
-     * 绑定用户
+    /**
+     * 商品列表
+     * author: zzx
+     * Date: 2018/11/20 0020
+     * Time: 13:34
+     * @return \think\response\Json
      */
     public function index()
     {
         try{
-//            $category = ProductCategoryModel::where(['level'=>1])->order([''])
-            $data = array_filter(input('','','trim'));
-            $data = ProductModel::getList($data);
+            $category_id = input('category_id','','trim');
+            if(empty($category_id)){
+                exception('错误请求');
+            }
+            $first_category = ProductCategoryModel::get(['id'=>$category_id]);
+            if(empty($first_category)){
+                exception('分类不存在');
+            }
+            $first_save_path = $first_category->save_path;
+
+            $category_con = [
+                ['parent_id','=',$category_id],
+                ['level','=',2],
+                ['is_show','=',1],
+            ];
+
+            $second_category = ProductCategoryModel::where($category_con)->order(['sort'=>'desc'])->column('id');
+            if(empty($second_category)){
+                return $this->succeed('操作成功',['pages'=>0]);
+            }
+
+            $con = [
+                'category_id' => $second_category,
+                'pages' => $second_category
+            ];
+
+            $data = ProductModel::appList($con);
+
+            $data['first_save_path'] = $first_save_path;
+
             return $this->succeed('操作成功',$data);
+
         }catch(\Exception $e){
             return $this->fail($e->getMessage());
         }
 
     }
 
+
+    /**
+     * 商品详情
+     * author: zzx
+     * Date: 2018/11/20 0020
+     * Time: 13:35
+     * @return \think\response\Json
+     */
+    public function detail()
+    {
+        try{
+            $id = input('id','','trim');
+            if(empty($id)){
+                exception('错误请求');
+            }
+            $product = ProductModel::getDetail($id);
+            $product->detail = strip_tags($product->detail);
+
+            return $this->succeed('操作成功',$product);
+
+        }catch(\Exception $e){
+            return $this->fail($e->getMessage());
+        }
+
+    }
 
 
 }
