bd1cd30ce5083775f4b0f7db711082b55cd79266
hollow <hollowcg@foxmail.com>
2018-12-20 13:38:30 +0800

    修改

--- application/app/controller/CartController.php
+++ application/app/controller/CartController.php
@@ -6,10 +6,20 @@ use app\common\model\CartModel;
 class CartController extends BaseController
 {
 
+    /**购物车列表
+     * author: zzx
+     * Date: 2018/12/20 0020
+     * Time: 09:46
+     * @return \think\response\Json
+     */
     public function index(){
         try{
             $input = input('','','trim');
-            $customer_id = $this->getCustomerId(['token'=>$input['token']]);
+            $customer = $this->getCustomer(['token'=>$input['token']]);
+            if($customer['code']==0){
+                exception($customer['msg']);
+            }
+            $customer_id = $customer['data']['id'];
             if(empty($customer_id)){
                 exception('用户不存在');
             }
@@ -18,7 +28,7 @@ class CartController extends BaseController
             if($cart['code']==0){
                 exception($cart['msg']);
             }
-
+            $cart['customer_type'] = $customer['data']->getData('type');
             return $this->succeed('操作成功',$cart);
 
         }catch(\Exception $e){
@@ -28,7 +38,7 @@ class CartController extends BaseController
 
 
     /**
-     * 列表页添加购物车
+     * 产品列表页添加购物车
      * author: zzx
      * Date: 2018/12/19 0019
      * Time: 16:07
@@ -70,7 +80,13 @@ class CartController extends BaseController
     }
 
 
-
+    /**
+     * 商品详情添加购物车
+     * author: zzx
+     * Date: 2018/12/20 0020
+     * Time: 11:17
+     * @return \think\response\Json
+     */
     public function add(){
         try{
             $data = input('','','trim');
@@ -92,6 +108,44 @@ class CartController extends BaseController
             }
             $cart_total_count = CartModel::countByCustomer($customer_id);
             return $this->succeed('操作成功',['count'=>$cart['count'],'cart_id'=>$cart['id'],'cart_total_count'=>$cart_total_count]);
+        }catch(\Exception $e){
+            return $this->fail($e->getMessage());
+        }
+    }
+
+
+
+    /**
+     * 产品列表页添加购物车
+     * author: zzx
+     * Date: 2018/12/19 0019
+     * Time: 16:07
+     * @return \think\response\Json
+     */
+    public function cartSave(){
+        try{
+            $post = input('','','trim');
+            $post['customer_id'] = $this->getCustomerId(['token'=>$post['token']]);
+            if($post['count']<=0){
+                $result = CartModel::where(['id'=>$post['id']])->delete();
+                if(!$result){
+                    throw new \think\Exception('操作失败');
+                }
+            }else {
+                $cart = CartModel::where(['id'=>$post['id']])->find();
+                if (empty($cart)) {
+                    exception('错误请求');
+                }
+                if(!$cart->force()->save(['count'=>$post['count']])){
+                    exception('操作失败');
+                }
+            }
+
+            $cart = CartModel::appList(['customer_id'=>$post['customer_id']]);
+            if($cart['code']==0){
+                exception($cart['msg']);
+            }
+            return $this->succeed('操作成功',$cart);
         }catch(\Exception $e){
             return $this->fail($e->getMessage());
         }
